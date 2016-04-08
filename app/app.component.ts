4ab1b7a8414bfbb725c8685754389fb8f1e15886
Ludi AKUE <ludi@a-r.io>
2016-04-08 15:18:29 +0200

    add Angular2 hero tutorial to git

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from 'angular2/core';
+import {HeroesComponent} from './heroes.component';
+import {HeroService} from './hero.service';
+
+
+@Component ({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <my-heroes></my-heroes>
+  `,
+   directives: [HeroesComponent],
+   providers: [HeroService]
+})
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES:Hero[] = [
+    { id:11, name: 'Mithra' },
+    { id:12, name: 'Udwin' },
+    { id:13, name: 'Zarathousthra' },
+    { id:14, name: 'Kerr' },
+    { id:15, name: 'Tyr' },
+    { id:16, name: 'Shaal' },
+    { id:17, name: 'Kean' },
+    { id:18, name: 'Latentr' },
+    { id:19, name: 'Waldorph' },
+    { id:20, name: 'Opossum' },
+];
